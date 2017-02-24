bd474a0d4ef95c7d0188794e42e7b8dc9dcd3614
Coty Beasley <coty@cotybeasley.name>
2017-02-24 14:09:45 -0600

    Styled items in grids

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
+// Init Model
 var mainModel = {};
+
+// Header Titles
+var header_value = 'Value (1-3)';
+var header_horizon = 'Horizon (1-3)';
+
+// Write Data From CSV
 d3.csv("data/data.csv", function(data) {
     mainModel = data;
     console.log(data);
     for (var i=0, len=data.length; i < len; ++i) {
         var item = data[i];
-        var valueClass = '.valueRow' + Math.floor(item['Value (1-3)']);
-        var horizonClass = '.horizon' + Math.floor(item['Horizon (1-3)']);
+        var valueClass = '.valueRow' + Math.floor(item[header_value]);
+        var horizonClass = '.horizon' + Math.floor(item[header_horizon]);
         var matrixAddress = valueClass + ' > ' + horizonClass;
-        $(matrixAddress).append('<div class="item">' + item.ID + '</div>');
+        $(matrixAddress).append('<div class="item id_' + item.ID + '"><span>' + item.ID + '</span></div>');
     }
 });
