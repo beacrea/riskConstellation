57fa052fd290987560e143bdd9999449f684de7f
Coty Beasley <coty@cotybeasley.name>
2017-02-24 12:54:52 -0600

    Parse data to empty divs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 d3.csv("data/data.csv", function(data) {
-    console.log(data[1]);
+    for (var i=0, len=data.length; i < len; ++i) {
+        $('#containerMain').append('<div>'+ data[i].ID +'</div>');
+        if (data[i].Category == "Configuration") {
+            console.log(data[i]);
+        }
+    }
 });
