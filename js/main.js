b6e828a8e5314e0afb20c0d6492c5a888cb612fe
Coty Beasley <coty@cotybeasley.name>
2017-02-24 13:04:59 -0600

    Assigned csv data to global var

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+var mainModel = {};
 d3.csv("data/data.csv", function(data) {
-    for (var i=0, len=data.length; i < len; ++i) {
-        $('#containerMain').append('<div>'+ data[i].ID +'</div>');
+    mainModel = data;
+    /*for (var i=0, len=data.length; i < len; ++i) {
         if (data[i].Category == "Configuration") {
             console.log(data[i]);
         }
-    }
+    }*/
 });
