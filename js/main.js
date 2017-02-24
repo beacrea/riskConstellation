1b61ea7e44ca5657f8c5fdb2592872a5972b5e50
Coty Beasley <coty@cotybeasley.name>
2017-02-24 13:47:43 -0600

    Created uniform grid with item population

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var mainModel = {};
 d3.csv("data/data.csv", function(data) {
     mainModel = data;
-    /*for (var i=0, len=data.length; i < len; ++i) {
-        if (data[i].Category == "Configuration") {
-            console.log(data[i]);
-        }
-    }*/
+    console.log(data);
+    for (var i=0, len=data.length; i < len; ++i) {
+        var item = data[i];
+        var valueClass = '.valueRow' + Math.floor(item['Value (1-3)']);
+        var horizonClass = '.horizon' + Math.floor(item['Horizon (1-3)']);
+        var matrixAddress = valueClass + ' > ' + horizonClass;
+        $(matrixAddress).append('<div class="item">' + item.ID + '</div>');
+    }
 });
